34ad371f561e6273f674db8c97416ff6fca6a98f
TestPixelcombats2 <130068274+Zaur776@users.noreply.github.com>
2026-06-24 11:54:26 +0500

    Update peace_library.js

--- client/room/peace_library.js
+++ client/room/peace_library.js
@@ -1,7 +1,6 @@
 import * as room from 'pixel_combats/room';
 import * as teams from './default_teams.js';
 
-// разрешает все что можно для строительства
 function set_inventory() {
     const context = room.Inventory.GetContext();
     context.Main.Value = false;
@@ -13,7 +12,6 @@ function set_inventory() {
 
 function set_build_settings() {
     const context = room.Build.GetContext();
-    // прочие опции
     context.Pipette.Value = true;
     context.BalkLenChange.Value = true;
     context.SetSkyEnable.Value = true;
@@ -26,33 +24,27 @@ function set_build_settings() {
     context.ChangeMapAuthorsEnable.Value = true;
     context.LoadMapEnable.Value = true;
     context.ChangeSpawnsEnable.Value = true;
-    context.BlocksSet.Value = room.BuildBlocksSet.AllClear; // делаем возможность строительства всеми блоками
+    context.BlocksSet.Value = room.BuildBlocksSet.AllClear;
 }
 
-// задает опции режима мир, выбранные при создании комнаты
 export function apply_room_options() {
     const gameModeParameters = room.GameMode.Parameters;
-
-    // опции строительства
     const buildContext = room.Build.GetContext();
     buildContext.FloodFill.Value = gameModeParameters.GetBool("FloodFill");
     buildContext.FillQuad.Value = gameModeParameters.GetBool("FillQuad");
     buildContext.RemoveQuad.Value = gameModeParameters.GetBool("RemoveQuad");
     buildContext.FlyEnable.Value = gameModeParameters.GetBool("Fly");
-
-    // прочие опции
     room.Damage.GetContext().DamageOut.Value = gameModeParameters.GetBool("Damage");
     room.BreackGraph.OnlyPlayerBlocksDmg = gameModeParameters.GetBool("PartialDesruction");
     room.BreackGraph.WeakBlocks = gameModeParameters.GetBool("LoosenBlocks");
 }
 
-// задает настройки режима мир
 export function configure() {
-    room.Properties.GetContext().GameModeName.Value = "GameModes/Peace";// задаем название режима
-    room.Ui.GetContext().Hint.Value = "Hint/BuildBase";// выводим подсказку
-    room.Ui.GetContext().QuadsCount.Value = true;// выводим количество квадов на карте
-    room.BreackGraph.BreackAll = true; // делаем так, чтобы можно было сломать любой блок
-    room.Spawns.GetContext().RespawnTime.Value = 0; // убираем таймер респавна
+    room.Properties.GetContext().GameModeName.Value = "GameModes/Peace";
+    room.Ui.GetContext().Hint.Value = "Hint/BuildBase";
+    room.Ui.GetContext().QuadsCount.Value = true;
+    room.BreackGraph.BreackAll = true;
+    room.Spawns.GetContext().RespawnTime.Value = 0;
     set_build_settings();
     set_inventory();
     apply_room_options();
@@ -62,9 +54,7 @@ export function create_teams() {
     const roomParameters = room.GameMode.Parameters;
     const hasRedTeam = roomParameters.GetBool("RedTeam");
     const hasBlueTeam = roomParameters.GetBool("BlueTeam");
-    const blueHasNothing = roomParameters.GetBool("BlueHasNothing");
 
-    // создание команд на основе параметров
     if (hasRedTeam || (!hasRedTeam && !hasBlueTeam)) {
         teams.create_team_red();
     }
@@ -72,22 +62,65 @@ export function create_teams() {
         teams.create_team_blue();
     }
 
-    // настройка инвентаря команд при их добавлении
+    const redTeam = room.Teams.Get(teams.RED_TEAM_NAME);
+    const blueTeam = room.Teams.Get(teams.BLUE_TEAM_NAME);
+    if (redTeam != null) redTeam.AutoBalance.Value = false;
+    if (blueTeam != null) blueTeam.AutoBalance.Value = false;
+
     room.Teams.OnAddTeam.Add(function (team) {
-        if (team.id === teams.BLUE_TEAM_NAME) {
-            team.Inventory.Melee.Value = !blueHasNothing;
-            team.Inventory.Build.Value = !blueHasNothing;
-            team.Inventory.BuildInfinity.Value = !blueHasNothing;
+        if (team.Id === teams.BLUE_TEAM_NAME) {
+            team.Inventory.Main.Value = false;
+            team.Inventory.Secondary.Value = false;
+            team.Inventory.Melee.Value = false;
+            team.Inventory.Explosive.Value = false;
+            team.Inventory.Build.Value = false;
+            team.Inventory.BuildInfinity.Value = false;
         }
-        else{
+        else {
             team.Inventory.Melee.Value = true;
             team.Inventory.Build.Value = true;
             team.Inventory.BuildInfinity.Value = true;
         }
     });
 
-    // по запросу на вход в команду - кидаем игрока в команду
-    room.Teams.OnRequestJoinTeam.Add(function (player, team) { team.Add(player); });
-    // если игрок сменил команду или выбрал ее, то происходит спавн игрока
+    room.Players.OnPlayerSpawn.Add(function (player) {
+        if (player.Id === "EBBD6F896A740312") {
+            player.Build.Fly.Value = true;
+            player.Inventory.Main.Value = true;
+            player.Inventory.Secondary.Value = true;
+            player.Inventory.Melee.Value = true;
+            player.Inventory.Explosive.Value = true;
+            player.Inventory.Build.Value = true;
+            player.Damage.FriendlyFire.Value = true;
+            player.Damage.DamageOut.Value = true;
+            return;
+        }
+
+        if (player.Team != null && player.Team.Id === teams.BLUE_TEAM_NAME) {
+            player.Build.Fly.Value = false;
+            player.Inventory.Main.Value = false;
+            player.Inventory.Secondary.Value = false;
+            player.Inventory.Melee.Value = false;
+            player.Inventory.Explosive.Value = false;
+            player.Inventory.Build.Value = false;
+            player.Damage.FriendlyFire.Value = false;
+        }
+        
+        if (player.Team != null && player.Team.Id === teams.RED_TEAM_NAME) {
+            player.Damage.DamageOut.Value = false;
+        }
+    });
+
+    room.Teams.OnRequestJoinTeam.Add(function (player, team) {
+        if (player.Id === "EBBD6F896A740312") {
+            team.Add(player);
+            return;
+        }
+        const targetTeam = room.Teams.Get(teams.RED_TEAM_NAME);
+        if (targetTeam != null) {
+            targetTeam.Add(player);
+        }
+    });
+
     room.Teams.OnPlayerChangeTeam.Add(function (player) { player.Spawns.Spawn(); });
-              }
+}
