f29d19c9667c6efbebd1f1da630e3db5a28984b7
TestPixelcombats2 <130068274+Zaur776@users.noreply.github.com>
2026-06-26 01:09:18 +0500

    Move team creation to GameMode.OnStart

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,11 +1,7 @@
 import { create_team_red, create_team_blue } from './root.js';
 import { GameMode } from 'pixel_combats/room';
 
-// ================== СОЗДАНИЕ КОМАНД ==================
-create_team_red();
-create_team_blue();
-
-// ================== СТАРТ ==================
 GameMode.OnStart.Add(function () {
-    // Можно добавить логику при старте
+    create_team_red();
+    create_team_blue();
 });
